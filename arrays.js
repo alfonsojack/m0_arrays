f47ab21d4b0ad1d899a168367558c3b47752f063
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-06-21 11:50:54 -0600

    Call 3 different array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var people = ["Joe", "Jason", "Jack", "Jonathan"]
 var ages = [21, 22, 23, 24, 25]
 var fancy = [true, true, false, true]
+people.pop();
+console.log(people)
+// This array method takes out the last element in the array
+ages.shift();
+console.log(ages)
+// This array method takes out the first element in the array
+fancy.push(true);
+console.log(fancy)
+// This array method adds a new element to the end of the array
