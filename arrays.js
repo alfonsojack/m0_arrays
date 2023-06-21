ad1ea049dcc8320ab8f1a7a54eb66dfd55de6dcf
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-06-21 11:58:48 -0600

    Demonstrate understanding of index positions

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ console.log(ages)
 fancy.push(true);
 console.log(fancy)
 // This array method adds a new element to the end of the array
+
+
+//Index positions count by 1 starting from 0. The following code should print "Jason" because it is in index position 1
+console.log(people[1])
+
+//The following code should print "24" because that is index position 2 after the array method removed the first element. It was originally in index position 3
+console.log(ages[2])
+
+//The code "console.log(ages[*number of index position*])" would print whatever element is in that index position.
+
+
